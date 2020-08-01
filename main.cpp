a7b12b6662bd0a924c5319af6563d0f80421ae85
love5yan <54840826+love5yan@users.noreply.github.com>
2020-08-01 21:18:15 +0800

    图的存储

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
 #include <stdio.h>
 
-typedef int ElemType;
+const int MaxVertexNum=100;
+typedef char VertexType ;
+typedef int EdgeType;
+typedef struct{
+    VertexType Vex[MaxVertexNum];//顶点表
+    EdgeType Edge[MaxVertexNum][MaxVertexNum];//邻接矩阵，边表
+    int vexnum,arcnum;//顶点数，弧数
+} MGraph;
+typedef struct ArcNode {//边表节点
+    int adjvex;//该边指向的顶点位置
+    ArcNode *next;//指向下一条弧的指针
+    //infoType
+} ArcNode;
+typedef struct VNode {//顶点表节点
+    VertexType data;//顶点信息
+    ArcNode *first;//第一个边
+} VNode, AdjList[MaxVertexNum];
 typedef struct {
-    ElemType *elem;
-    int TableLen;
-} SSTable;
+    AdjList vertices;//邻接表
+    int vexnum, arcnum;//图的顶点数和弧数
+} AlGraph;
 
-
-
-int main() {
+int main(){
 
 }
